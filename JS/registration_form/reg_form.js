a82c2b7005870bab4b6a768851e1539e90524503
NataliyaChe <happylibrarygeek@gmail.com>
2022-10-19 14:54:28 +0300

    final fix

--- JS/registration_form/reg_form.js
+++ JS/registration_form/reg_form.js
@@ -7,9 +7,6 @@ const login_lbl = document.querySelector('.login_lbl');
 const email_lbl = document.querySelector('.email_lbl');
 const password_lbl = document.querySelector('.password_lbl');
 
-// const dataArr = []
-// localStorage.setItem('personArr', JSON.stringify(dataArr))
-
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -25,7 +22,6 @@ form.addEventListener('submit', (event) => {
     } else {
         errorName.style.display = 'none';
         login_lbl.style.display = 'block';
-        isError = false
     };
 
     if(emailValidate(dataObj.email)) {
@@ -35,7 +31,6 @@ form.addEventListener('submit', (event) => {
     } else {
         errorEmail.style.display = 'none';
         email_lbl.style.display = 'block';
-        isError = false
     };
 
     if(dataObj.password !== dataObj.password_rpt) {
@@ -45,7 +40,6 @@ form.addEventListener('submit', (event) => {
     } else {
         errorPassword.style.display = 'none';
         password_lbl.style.display = 'block';
-        isError = false
     };
 
     const users = JSON.parse(localStorage.getItem('usersArr')) ?? []
@@ -59,17 +53,14 @@ form.addEventListener('submit', (event) => {
     } else {
         wrongEmail.style.display = 'none';
         email_lbl.style.display = 'block';
-        isError = false
     }; 
    
     if(isError == false) {
+        ('final', isError);
         users.push(dataObj)
         localStorage.setItem('usersArr', JSON.stringify(users))
     }
-    // users.push(dataObj)
-    // localStorage.setItem('usersArr', JSON.stringify(users))
-    }
-);
+});
 
 function emailValidate(login) {
   return !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(login);
@@ -81,31 +72,5 @@ function nameValidate(email) {
 
 const findDuplicateEmail = (arr, email) => arr.find(item => email === item.email)
 
-// function findEmail(arr, email) {
-//      return arr.find(item => email === item.email)
-// }
-
-// findEmail = () => console.log('Hello');
-// findEmail()
-// function findEmail(arr, email) {
-//     arr.forEach(function(item){ 
-//         console.log('item', item.email);  
-//         email === item.email
-//         })
-//     return !email === item.email
-// }
-
 // localStorage.clear()
 
-// const storageUser = JSON.parse(localStorage.getItem('person'))
-//     console.log('starage', storageUser);
-
-// let storageLogin = login
-//     user[storageLogin] = login.value
-
-
-// function checkPassword() {
-//     if (password.value !== password_rpt.value) {
-//         return false
-//     }
-// };
