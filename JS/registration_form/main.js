ecf740afd3d9ef02d984b5b6b92784984869d0bb
NataliyaChe <happylibrarygeek@gmail.com>
2022-10-21 15:58:53 +0300

    main js fixed

--- JS/registration_form/main.js
+++ JS/registration_form/main.js
@@ -10,26 +10,16 @@ console.log('users', users);
 
 title.innerHTML = `Hello ${matchUser.login}!`
 
-// const findDuplicateLogin = (arr, login) => arr.find(item => login === item.login)
-
-// let thisUser = findDuplicateLogin(users, matchUser.login) 
-// console.log('obj', thisUser);
-
-// const taskList = [];
-
-
-// const taskStorage = JSON.parse(localStorage.getItem('taskArr')) ?? []
-
 task.addEventListener('keypress', event => {
     if(event.code === 'Enter') {
         for(let i = 0; i < users.length; i++) {
-            console.log('users.task for', users.task);
-            if(users.login === matchUser.login) {
-                console.log('users.task if', users.task);
-                users.task.push(task.value)
+            console.log('users.task for', users[i].task);
+            if(users[i].login === matchUser.login) {
+                console.log('users.task if', users[i].task);
+                users[i].task.push(task.value)
+                localStorage.setItem('usersArr', JSON.stringify(users))
             }
         }
-        localStorage.setItem('users', JSON.stringify(users))
         let item = document.createElement('li');
         item.innerHTML = task.value;
         list.append(item);
@@ -38,37 +28,6 @@ task.addEventListener('keypress', event => {
     }
 });
 
-// function makeListFromStorage(arr) {
-    // for(let i = 0; i < arr.length; i++) {
-    //     let storageItem = document.createElement('li');
-    //     storageItem.innerHTML = arr[i];
-    //     list.append(storageItem);
-    // }
-// };
-// makeListFromStorage(matchUser.task)
-
-// task.addEventListener('keypress', event => {
-//     if(event.code === 'Enter') {
-//         taskList.push(task.value)
-//         thisUser.task = taskList
-        // localStorage.setItem('thisUser', JSON.stringify(thisUser))
-        // console.log('thisUser', thisUser);
-        // let item = document.createElement('li');
-        // item.innerHTML = task.value;
-        // list.append(item);
-        // task.value = '';
-//     }
-// });
-
-// const findDuplicateEmail = (arr, login) => arr.find(item => login === item.login)
-
-// btn.addEventListener('click', (event) => {
-//     console.log('click', task.value);
-//     let item = document.createElement('li');
-//     item.innerHTML = task.value;
-//     list.append(item);
-//     task.value = '';
-// })
 
 // document.onkeydown = () => {
 //     if(e.keyCode === 13) {
