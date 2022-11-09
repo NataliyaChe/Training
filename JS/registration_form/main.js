30ae11e50f9860544092f689378d0464cb1a0bb5
NataliyaChe <happylibrarygeek@gmail.com>
2022-11-09 20:31:25 +0200

    post editing logic fixed

--- JS/registration_form/main.js
+++ JS/registration_form/main.js
@@ -1,10 +1,9 @@
 import {Storage} from "./utils.js";
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 const title = document.querySelector('.main-title');
 const postInp = document.querySelector('.task');
 const list = document.querySelector('.list');
-
-const postDate = new Date();
 
 // const matchUser = JSON.parse(localStorage.getItem('matchUser'));
 // const users = JSON.parse(localStorage.getItem('usersArr'));
@@ -17,34 +16,30 @@ if(matchUser === null) {
 }
 
 title.innerHTML = `Hello ${matchUser.name}!`;
-for(const userItem of users) {
-    if(userItem.name === matchUser.name) {
-        const userPosts = userItem.posts;
-        for(let i = 0; i < userPosts.length; i++) {
-            const liItem = document.createElement('li');
-            liItem.className = 'post-item';
-            liItem.innerHTML = `<p class="message">${userPosts[i].message}</p>  
-            <p>${userPosts[i].date}</p>`;
-            list.append(liItem);
-        }
-        
-    }
-    
-}
+  
+const user = users.find(item => item.email === matchUser.email);
 
+for(let i = 0; i < user.posts.length; i++) {
+    const liItem = document.createElement('li');
+    liItem.className = 'post-item';
+    liItem.innerHTML = `<p data-id="${user.posts[i].id}" class="message">${user.posts[i].message}</p>  
+    <p>${user.posts[i].date}</p>`;
+    list.append(liItem);
+}
+    
 postInp.addEventListener('keypress', event => {
     if(event.code === 'Enter') {
-        for(const userItem of users) {
-            if(userItem.name === matchUser.name) {
-                userItem.posts.push({message: postInp.value, date: postDate, editDate: checkDate});
-                // localStorage.setItem('usersArr', JSON.stringify(users));
-                storage.setItem('usersArr', users);
-            }
+        const newPost = {id: uuidv4(), message: postInp.value, date: new Date()}
+        user.posts.push(newPost);
+        function checkDate() {
+            
         }
+        // localStorage.setItem('usersArr', JSON.stringify(users));
+        storage.setItem('usersArr', users);
+               
         const liItem = document.createElement('li');
         liItem.className = 'post-item';
-        liItem.innerHTML = `<p class="message">${postInp.value}</p> 
-        <p>${postDate}</p> <p></p>`;
+        liItem.innerHTML = `<p data-id="${newPost.id}" class="message">${postInp.value}</p> <p>${newPost.date}</p> <p></p>`;
         list.append(liItem);
         postInp.value = '';
     }
@@ -58,25 +53,28 @@ postMessages.forEach(function(message) {
 })
 
 function liChange(event) {
-    const liParent = event.target.parentNode
+    const liParent = event.target.parentNode;
+    liParent.setAttribute("id", `${event.target.dataset.id}`);
     const editItem = document.createElement('textarea');
     editItem.className = 'editForm';
     editItem.innerHTML = `${event.target.innerHTML}`;
     liParent.replaceChild(editItem, event.target);
     editItem.focus();
     editItem.selectionStart = editItem.value.length;
-    editItem.addEventListener('focusout', liReturn)
+    editItem.addEventListener('focusout', liReturn)   
 }
 
 function liReturn(event) {
-    const liParent = event.target.parentNode
+    const liParent = event.target.parentNode;
+    const editedPost = user.posts.find(item => item.id === liParent.id)
     const pItem = document.createElement('p');
     pItem.className = 'message';
     pItem.innerHTML = `${event.target.value}`;
     liParent.replaceChild(pItem, event.target);
     pItem.addEventListener('click', liChange);
-    liParent.editDate = postDate 
-    console.log('usersArr', users, liParent); 
+    editedPost.message = pItem.innerHTML;
+    storage.setItem('usersArr', users);
+    // liParent.editDate = postDate 
 }
 
 // function addElement(event, itemEl, tag, className, content) {
@@ -93,3 +91,5 @@ function liReturn(event) {
 //     showElement.style.display = 'block';
 //     hideElement.style.display = 'none'; 
 // }
+
+// localStorage.clear()
